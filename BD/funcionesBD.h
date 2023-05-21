a75f78b83a06571e445d4f9449629b1d7e71017a
Guillermo Diaz <g.diaz@opendeusto.es>
2023-05-21 14:59:55 +0200

    Updates

---
+++ BD/funcionesBD.h
@@ -0,0 +1,13 @@
+/*
+ * funcionesBD.h
+ *
+ *  Created on: 21 may 2023
+ *      Author: Usuario
+ */
+
+#ifndef FUNCIONESBD_H_
+#define FUNCIONESBD_H_
+
+
+
+#endif /* FUNCIONESBD_H_ */
---
+++ cliente.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "cliente.h"
+
+void imprimirCliente(Cliente c)
+{
+	printf("DNI: %s Nombre: %s Apellido: %s Mail: %s, Telefono %s",c.dni,c.nombre,c.apellido,c.correo,c.telefono);
+}
+
